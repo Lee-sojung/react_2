c83f65bbc18197bf7428a8a1c9b1d5f7aa674441
Lee-sojung <thwjd3698@gmail.com>
2021-12-21 10:52:00 +0900

    step2 반복컨텐츠

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import './css/style.css';
+import Footer from './components/Footer.js';
+import {Logo,Menu} from './components/Header.js';
+
+const name1 = "DAVID";
 
 function App() {
 	return (
 		<div className="App">
-			<div className="btns">
-				<li>button1</li>
-				<li>button2</li>
-			</div>
+			{/*전체 레이아웃*/}
+			<figure>
+				<Logo />
+				<Menu />
+
+				<section>
+					<article>
+						<div className="inner">
+							{name1}
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							ANDY
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							MICHAEL
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							JULIA
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							CHRISTOPER
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							EMMA
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							PAUL
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							CATHERINE
+						</div>
+					</article>
+				</section>
+				
+				<Footer />
+			</figure>
 
 		</div>
 	);
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+        <p>2022 DECODELAB &copy; ALL RIGHTS RESERVED.</p>
+    )
+}
+
+export default Footer;
+
